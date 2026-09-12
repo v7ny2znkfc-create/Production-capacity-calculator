1b0672163edfa39eae1be453ac3264ac74e6f902
v7ny2znkfc-create <v7ny2znkfc@privaterelay.appleid.com>
2026-09-12 14:07:09 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='production-capacity-v5-4';
+const CACHE_NAME='production-capacity-v5-5';
 const APP_SHELL=['./','./index.html','./config.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))));self.clients.claim();});
